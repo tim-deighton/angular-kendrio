92c7c380e8d06dc91bfacc5098a12358f42aaa90
tim-deighton <tim.warp@gmail.com>
2019-06-21 11:49:09 +0100

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
   jsonSchema: any;
   uiSchema: any;
   fields: FormlyFieldConfig[]
-
+  
   constructor(
     private formlyJsonschema: FormlyJsonschema,
     private formService: KendraioFormService
@@ -38,13 +38,10 @@ export class AppComponent {
       });
   }
 
-
   submit() {
     alert(JSON.stringify(this.model));
   }
 }
-
-
 /**  Copyright 2018 Google Inc. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
     can be found in the LICENSE file at http://angular.io/license */
