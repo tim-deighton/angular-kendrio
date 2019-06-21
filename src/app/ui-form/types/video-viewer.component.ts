3181c5daa77aa8b35928b312f6e20a9635750d72
tim-deighton <tim.warp@gmail.com>
2019-06-21 13:40:19 +0100

    1 file modified

--- src/app/ui-form/types/video-viewer.component.ts
+++ src/app/ui-form/types/video-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
 import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
 
@@ -7,29 +7,36 @@ import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser
   selector: 'formly-field-video-viewer',
   template: `
 
- 
- xxx{{ to | json }}xxx
-
-
-
+<div>
+<label>{{to.label}}</label>
+</div>
 <iframe class="e2e-iframe-trusted-src" width="640" height="390" [src]="videoUrl"></iframe>
-    <input  type="text"  [textMask]="{mask: numberMask}" [formControl]="formControl" [formlyAttributes]="field">
+    <input  type="text" [formControl]="formControl" [formlyAttributes]="field">
 
 
  `,
 })
 // tslint:disable-next-line: component-class-suffix
 export class FormlyFieldVideoViewer extends FieldType {
+  @Input() 
+  formControl;
+
+videoUrl: any;
   constructor(
    private sanitizer: DomSanitizer
     ) {
-      super()
+      super();
+      console.log()
     }
 
+ ngOnInit() {
+console.log(this.formControl.value)
+this.videoUrl  =  this.sanitizer.bypassSecurityTrustResourceUrl(this.formControl.value);
+ }
+
+// innerHTML: string =  "<iframe width='560' height='315' src='https://www.youtube.com/embed/YA9N4nsAxZo' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
 
 
-innerHTML: string =  "<iframe width='560' height='315' src='https://www.youtube.com/embed/YA9N4nsAxZo' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
-videoUrl: any  = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/YA9N4nsAxZo');
 
 // updateVideoUrl(id: string) {
   // Appending an ID to a YouTube URL is safe.
